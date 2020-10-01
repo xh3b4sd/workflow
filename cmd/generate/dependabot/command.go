08ddc1db6b6f65e9adb3ee9b287e3d0b1e9b7958
Tim Schindler <xh3b4sd@gmail.com>
2020-10-01 23:14:29 +0200

    update workflows

--- cmd/generate/dependabot/command.go
+++ cmd/generate/dependabot/command.go
@@ -7,8 +7,30 @@ import (
 )
 
 const (
-	name        = "dependabot"
-	description = "Generate a dependabot workflow for e.g. golang and docker."
+	name  = "dependabot"
+	short = "Generate a dependabot workflow for e.g. golang and docker."
+	long  = `Generate a dependabot workflow for e.g. golang and docker. The dependabot
+workflow for golang includes a separate action of executing "go mod tidy" due
+to some dependabot limitations. This limitation requires automated fixing and
+therefore dedicated push access. The explicitly authorized push access is
+necessary in order to trigger another execution of the "go build" workflow
+after we fixed the go.mod and go.sum files. Due to these implementation
+details we setup deploy keys in each repository via the "red" command line
+tool. More information about the tool can be found at github.com/xh3b4sd/red.
+For each repository using the dependabot workflow the following command must
+be used in order to generate deploy keys and the associated GPG password.
+
+    red generate keys -d .github/asset
+
+The GPG encrypted private key must be put into the ".github/asset" directory.
+During each build the GPG encrypted private key is decrypted within the build
+container and used to setup the local SSH agent for the explicit push access.
+
+For the decryption of the GPG encrypted private key a password is required.
+This password gets generated together with the public and private key. The
+password must be made available to each repository as secret using the name
+"RED_GPG_PASS".
+	`
 )
 
 type Config struct {
@@ -31,8 +53,8 @@ func New(config Config) (*cobra.Command, error) {
 
 		c = &cobra.Command{
 			Use:   name,
-			Short: description,
-			Long:  description,
+			Short: short,
+			Long:  long,
 			RunE:  r.Run,
 		}
 
--- cmd/generate/dependabot/flag.go
+++ cmd/generate/dependabot/flag.go
@@ -1,21 +1,36 @@
 package dependabot
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 	"github.com/xh3b4sd/tracer"
+
+	"github.com/xh3b4sd/workflow/pkg/golang"
 )
 
 type flag struct {
 	Reviewers []string
+	Version   string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringSliceVarP(&f.Reviewers, "reviewers", "r", []string{}, "Reviewers assigned to dependabot PRs, e.g. xh3b4sd. Works with github usernames and teams.")
+	cmd.Flags().StringVarP(&f.Version, "version", "v", golang.Version, "Golang version to set in, e.g. go.mod.")
 }
 
 func (f *flag) Validate() error {
 	if len(f.Reviewers) == 0 {
 		return tracer.Maskf(invalidFlagError, "-r/--reviewers must not be empty")
+	}
+
+	if f.Version == "" {
+		return tracer.Maskf(invalidFlagError, "-v/--version must not be empty")
+	}
+
+	s := strings.Split(f.Version, ".")
+	if len(s) != 3 {
+		return tracer.Maskf(invalidFlagError, "-v/--version must have 3 parts like 1.15.2")
 	}
 
 	return nil
