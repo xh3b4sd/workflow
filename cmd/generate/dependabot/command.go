08f8c9207af042faec5139736b6d9ad7d12efe84
Tim Schindler <xh3b4sd@gmail.com>
2020-10-15 23:04:42 +0200

    improve help usage

--- cmd/generate/dependabot/command.go
+++ cmd/generate/dependabot/command.go
@@ -12,13 +12,14 @@ const (
 	long  = `Generate a dependabot workflow for e.g. golang and docker. The dependabot
 workflow for golang includes a separate action of executing "go mod tidy" due
 to some dependabot limitations. This limitation requires automated fixing and
-therefore dedicated push access. The explicitly authorized push access is
-necessary in order to trigger another execution of the "go build" workflow
-after we fixed the go.mod and go.sum files. Due to these implementation
-details we setup deploy keys in each repository via the "red" command line
-tool. More information about the tool can be found at github.com/xh3b4sd/red.
-For each repository using the dependabot workflow the following command must
-be used in order to generate deploy keys and the associated GPG password.
+therefore dedicated push access to the configured repository. The explicitly
+authorized push access is necessary in order to trigger another execution of
+the "go build" workflow after we fixed the go.mod and go.sum files. Due to
+these implementation details we setup deploy keys in each repository via the
+"red" command line tool. More information about the tool can be found at
+github.com/xh3b4sd/red. For each repository using the dependabot workflow the
+following command must be used in order to generate deploy keys and the
+associated GPG password.
 
     red generate keys -d .github/asset
 
@@ -26,11 +27,22 @@ The GPG encrypted private key must be put into the ".github/asset" directory.
 During each build the GPG encrypted private key is decrypted within the build
 container and used to setup the local SSH agent for the explicit push access.
 
-For the decryption of the GPG encrypted private key a password is required.
-This password gets generated together with the public and private key. The
-password must be made available to each repository as secret using the name
-"RED_GPG_PASS".
-	`
+    .github/asset/id_rsa.enc
+
+The plain text public key must be added as deploy key with write access to
+the configured repository. During builds of dependabot pull requests the
+configured deploy key verifies that the configured private key is allowed to
+push changes during builds.
+
+    .github/asset/id_rsa.pub
+
+For the decryption of the GPG encrypted private key during builds, a password
+is required. This password gets generated together with the RSA public and
+private key. The password must be set as secret to the configured repository.
+The secret name must be as follows.
+
+    RED_GPG_PASS
+`
 )
 
 type Config struct {
