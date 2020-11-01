188432725c56709d1f8276a8f80af4ae676a1e6c
Tim Schindler <xh3b4sd@gmail.com>
2020-11-01 22:20:18 +0100

    update workflows (#31)

--- cmd/generate/golang/template.go
+++ cmd/generate/golang/template.go
@@ -38,7 +38,7 @@ jobs:
           test -z $(gofmt -l -s .)
 
       - name: "Check Go Linters"
-        uses: "golangci/golangci-lint-action@v2.2.1"
+        uses: "golangci/golangci-lint-action@v2.3.0"
         with:
-          version: "v1.31"
+          version: "v1.32.1"
 `
