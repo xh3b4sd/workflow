d062b04590ea2f5c69aa329ba0f34727d3338dc0
Tim Schindler <xh3b4sd@gmail.com>
2020-11-01 23:01:52 +0100

    add update subcommand

---
+++ cmd/update/all/command.go
@@ -0,0 +1,39 @@
+package all
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/xh3b4sd/logger"
+	"github.com/xh3b4sd/tracer"
+)
+
+const (
+	name  = "all"
+	short = "Update all github workflows to the latest version."
+	long  = `Update all github workflows to the latest version.`
+)
+
+type Config struct {
+	Logger logger.Interface
+}
+
+func New(config Config) (*cobra.Command, error) {
+	if config.Logger == nil {
+		return nil, tracer.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	var c *cobra.Command
+	{
+		r := &runner{
+			logger: config.Logger,
+		}
+
+		c = &cobra.Command{
+			Use:   name,
+			Short: short,
+			Long:  long,
+			RunE:  r.Run,
+		}
+	}
+
+	return c, nil
+}
