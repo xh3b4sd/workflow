a4e11a5b69c16f4e0b574d81a48f9f97a8661377
Tim Schindler <xh3b4sd@gmail.com>
2020-11-03 19:42:58 +0100

    add typescript workflow for npm package publishing (#41)

--- cmd/create/command.go
+++ cmd/create/command.go
@@ -11,6 +11,7 @@ import (
 	"github.com/xh3b4sd/workflow/cmd/create/golang"
 	"github.com/xh3b4sd/workflow/cmd/create/grpcgo"
 	"github.com/xh3b4sd/workflow/cmd/create/grpcts"
+	"github.com/xh3b4sd/workflow/cmd/create/typescript"
 )
 
 const (
@@ -102,6 +103,18 @@ func New(config Config) (*cobra.Command, error) {
 		}
 	}
 
+	var typescriptCmd *cobra.Command
+	{
+		c := typescript.Config{
+			Logger: config.Logger,
+		}
+
+		typescriptCmd, err = typescript.New(c)
+		if err != nil {
+			return nil, tracer.Mask(err)
+		}
+	}
+
 	var c *cobra.Command
 	{
 		r := &runner{
@@ -121,6 +134,7 @@ func New(config Config) (*cobra.Command, error) {
 		c.AddCommand(golangCmd)
 		c.AddCommand(grpcgoCmd)
 		c.AddCommand(grpctsCmd)
+		c.AddCommand(typescriptCmd)
 	}
 
 	return c, nil
---
+++ cmd/create/typescript/template.go
@@ -0,0 +1,54 @@
+package typescript
+
+const templateTypescript = `#
+# Do not edit. This file was generated via the "workflow" command line tool.
+# More information about the tool can be found at github.com/xh3b4sd/workflow.
+#
+#     {{ .Command }}
+#
+# Note that this workflow has several requirements in order to function
+# correctly throughout the development process. See the desired sequence of
+# steps below.
+#
+#     1. A developer bumps the semver version within the package.json.
+#     2. A developer creates a new github release according to step 1.
+#     3. The workflow builds and publishes the new npm package.
+#
+
+# to be up to date. Further
+
+name: "typescript"
+
+on:
+  release:
+    types:
+      - "created"
+
+jobs:
+  npm-publish:
+    runs-on: "ubuntu-latest"
+    steps:
+
+      - name: "Checkout Git Project"
+        uses: actions/checkout@v2
+
+      - name: "Setup Node Env"
+        uses: "actions/setup-node@v1"
+        with:
+          node-version: "{{ .Version.Node }}"
+          registry-url: "https://npm.pkg.github.com"
+
+      - name: "Install NPM Dependencies"
+        run: |
+          npm install
+
+      - name: "Build NPM Package"
+        run: |
+          npm run build
+
+      - name: "Publish NPM Package"
+        env:
+          NODE_AUTH_TOKEN: "${{ "{{" }} secrets.GITHUB_TOKEN {{ "}}" }}"
+        run: |
+          npm publish
+`
