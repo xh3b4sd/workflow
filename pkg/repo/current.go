a37f1305a27c4dd07876c848e350bf786a696281
Tim Schindler <xh3b4sd@gmail.com>
2020-11-09 19:36:53 +0100

    relax current repo detection and make usage template more resilient

--- pkg/repo/current.go
+++ pkg/repo/current.go
@@ -5,15 +5,17 @@ import (
 	"strings"
 )
 
-// Current tries to lookup the name of the current Github repository. If Current
-// fails to determine the absolute file system path of the program's location it
-// panics. Consider the current absolut path.
+// Current tries to lookup the location of the current Github repository. If
+// Current fails to determine the absolute file system path of the program's
+// location it panics. Consider the current absolut path.
 //
 //     /Users/xh3b4sd/go/src/github.com/xh3b4sd/workflow/
+//     /Users/xh3b4sd/projects/xh3b4sd/apischema
 //
-// The resulting Github repository returned would be the following.
+// The resulting Github repository returned would be the following respectively.
 //
 //     github.com/xh3b4sd/workflow
+//     github.com/xh3b4sd/apischema
 //
 func Current() string {
 	a, err := filepath.Abs(".")
@@ -21,24 +23,40 @@ func Current() string {
 		panic(err)
 	}
 
-	var c int
-	var p []string
-	var t bool
-	for _, s := range strings.Split(a, "/") {
-		if t {
-			p = append(p, s)
-			c++
-		}
+	return current(a)
+}
 
-		if s == "github.com" {
-			p = append(p, s)
-			t = true
-		}
+func current(a string) string {
+	l := strings.Split(a, "/")
 
-		if c >= 2 {
-			break
+	{
+		var c int
+		var p []string
+		var t bool
+
+		for _, s := range l {
+			if t {
+				p = append(p, s)
+				c++
+			}
+
+			if s == "github.com" {
+				p = append(p, s)
+				t = true
+			}
+
+			if c >= 2 {
+				return filepath.Join(p...)
+			}
 		}
 	}
 
-	return filepath.Join(p...)
+	{
+		var p []string
+
+		p = append(p, "github.com")
+		p = append(p, l[len(l)-2:]...)
+
+		return filepath.Join(p...)
+	}
 }
