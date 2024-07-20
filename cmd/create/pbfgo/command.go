a5a78a608ae47cb009ee867f4717d8b846dd5d8c
xh3b4sd <xh3b4sd@gmail.com>
2024-07-20 15:16:57 +0200

    update go version

--- cmd/create/pbfgo/command.go
+++ cmd/create/pbfgo/command.go
@@ -11,15 +11,21 @@ const (
 	short = "Create a protocol buffer workflow for golang code generation."
 	long  = `Create a protocol buffer workflow for golang code generation. The workflow
 generated here works in a setup of two Github repositories. Call them apischema
-and gocode. The workflow generated with the following command is added to the
+and apigocode. The workflow generated with the following command is added to the
 apischema repository.
 
-    workflow create pbfgo -o xh3b4sd -r gocode
+    workflow create pbfgo -o fancy-organization -r apigocode
 
 In order to make the workflow function correctly a deploy key must be generated
 and distributed. The public key is added as deploy key with write access to the
-gocode repository. The private key is added as Github Action Secret to the
-apischema repository. See the link below for more information.
+apigocode repository. The private key is added as Github Action Secret to the
+apischema repository, call it SSH_DEPLOY_KEY_APIGOCODE. A new key pair can be
+generated like shown below.
+
+    ssh-keygen -t ed25519 -C your@email.com
+
+More information about the Github Action used to push changes from one
+repository to another can be found following the link below.
 
     https://github.com/cpina/github-action-push-to-another-repository
 `
